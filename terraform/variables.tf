75f4a0f0cc4d5505870a473a26a194cb21cac6b8
RareSonal <sonali.mandrupkar@gmail.com>
2025-07-08 20:36:03 +0530

    updated variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,6 @@
 variable "aws_region" {
-  description = "The AWS region to deploy resources in"
+  description = "The AWS region to deploy resources in (e.g., us-east-1)"
   type        = string
-  default     = "us-east-1"
 }
 
 variable "key_name" {
@@ -12,35 +11,29 @@ variable "key_name" {
 variable "db_instance_identifier" {
   description = "Identifier of the existing PostgreSQL RDS instance"
   type        = string
-  default     = "stockwishlist-postgres"
 }
 
 variable "db_username_ssm_path" {
-  description = "SSM Parameter Store path for DB username"
+  description = "SSM Parameter Store path for the DB username"
   type        = string
-  default     = "/stockwishlist/db_username"
 }
 
 variable "db_password_ssm_path" {
-  description = "SSM Parameter Store path for DB password"
+  description = "SSM Parameter Store path for the DB password"
   type        = string
-  default     = "/stockwishlist/db_password"
 }
 
 variable "vpc_id" {
   description = "VPC ID to launch the EC2 instance in"
   type        = string
-  default     = "vpc-0b6987d09fe5c1577"
 }
 
 variable "public_subnet_id" {
   description = "Subnet ID to launch the EC2 instance in"
   type        = string
-  default     = "subnet-0d851f3b40055b56d" 
 }
 
 variable "security_group_id" {
   description = "Security Group ID to associate with the EC2 instance"
   type        = string
-  default     = "sg-0965338ad1360b60a" 
 }
