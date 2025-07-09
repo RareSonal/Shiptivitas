3d7273d13a140d12b722593fc3506a3880f27725
RareSonal <sonali.mandrupkar@gmail.com>
2025-07-09 18:41:41 +0530

    updated trimmed input to existing data

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,13 +1,13 @@
 data "aws_db_instance" "existing_rds" {
-  db_instance_identifier = var.db_instance_identifier
+  db_instance_identifier = trimspace(var.db_instance_identifier)
 }
 
 data "aws_ssm_parameter" "db_username" {
-  name            = var.db_username_ssm_path
+  name            = trimspace(var.db_username_ssm_path)
   with_decryption = true
 }
 
 data "aws_ssm_parameter" "db_password" {
-  name            = var.db_password_ssm_path
+  name            = trimspace(var.db_password_ssm_path)
   with_decryption = true
 }
