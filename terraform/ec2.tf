6190158714e74e2c55fff85189e3eddf69fc1ba4
RareSonal <sonali.mandrupkar@gmail.com>
2025-07-10 21:58:46 +0530

    checking resource existance

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,5 +1,3 @@
-
-
 data "aws_ami" "amazon_linux" {
   owners      = ["amazon"]
   most_recent = true
@@ -20,52 +18,50 @@ data "template_file" "user_data" {
   }
 }
 
-# --- IAM Role for EC2 to access SSM ---
+# IAM Role for EC2 to access SSM
 resource "aws_iam_role" "ec2_ssm_role" {
-  count = var.create_ec2_ssm_role ? 1 : 0
+  count = var.create_iam_role ? 1 : 0
 
   name = "shiptivitas-ec2-ssm-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        },
-        Action = "sts:AssumeRole"
-      }
-    ]
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
   })
 }
 
 resource "aws_iam_role_policy_attachment" "ssm_policy" {
-  count      = var.create_ec2_ssm_role ? 1 : 0
+  count      = var.create_iam_role ? 1 : 0
   role       = aws_iam_role.ec2_ssm_role[0].name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMReadOnlyAccess"
 }
 
 resource "aws_iam_role_policy_attachment" "ssm_core" {
-  count      = var.create_ec2_ssm_role ? 1 : 0
+  count      = var.create_iam_role ? 1 : 0
   role       = aws_iam_role.ec2_ssm_role[0].name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
 resource "aws_iam_instance_profile" "ec2_ssm_profile" {
-  count = var.create_ec2_ssm_role ? 1 : 0
+  count = var.create_iam_profile ? 1 : 0
   name  = "shiptivitas-ec2-ssm-profile"
-  role  = aws_iam_role.ec2_ssm_role[0].name
+  role  = var.create_iam_role ? aws_iam_role.ec2_ssm_role[0].name : "shiptivitas-ec2-ssm-role"
 }
 
-# --- EC2 Instance ---
+# EC2 Instance
 resource "aws_instance" "shiptivitas_api" {
   ami                         = data.aws_ami.amazon_linux.id
   instance_type               = "t2.micro"
   key_name                    = var.key_name
   subnet_id                   = trimspace(var.public_subnet_id)
   vpc_security_group_ids      = [trimspace(var.security_group_id)]
-  iam_instance_profile        = var.create_ec2_ssm_role ? aws_iam_instance_profile.ec2_ssm_profile[0].name : "shiptivitas-ec2-ssm-profile"
+  iam_instance_profile        = var.create_iam_profile ? aws_iam_instance_profile.ec2_ssm_profile[0].name : "shiptivitas-ec2-ssm-profile"
   associate_public_ip_address = true
   user_data                   = data.template_file.user_data.rendered
 
