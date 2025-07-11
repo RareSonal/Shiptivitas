6c4bcf351924d299b51b6272d7183f77f40b2211
RareSonal <sonali.mandrupkar@gmail.com>
2025-07-11 16:51:50 +0530

    checking resource existance

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,14 +1,19 @@
 output "s3_bucket" {
   description = "Name of the S3 bucket (if created)"
-  value       = try(aws_s3_bucket.shiptivitas_frontend[0].bucket, "")
+  value       = var.create_s3_bucket ? aws_s3_bucket.shiptivitas_frontend[0].bucket : null
 }
 
 output "cloudfront_url" {
   description = "Domain name of the CloudFront distribution"
-  value       = var.use_existing_cdn ? try(data.aws_cloudfront_distribution.existing[0].domain_name, "") : try(aws_cloudfront_distribution.cdn[0].domain_name, "")
+  value       = try(
+    var.use_existing_cdn
+      ? data.aws_cloudfront_distribution.existing[0].domain_name
+      : aws_cloudfront_distribution.cdn[0].domain_name,
+    null
+  )
 }
 
 output "backend_ec2_public_ip" {
   description = "Public IP of the backend EC2 instance"
-  value       = try(aws_instance.shiptivitas_api.public_ip, "")
+  value       = var.create_ec2 ? aws_instance.shiptivitas_api[0].public_ip : null
 }
