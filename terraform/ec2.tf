175c35aa7a57b322e9cec005007789ba56182408
RareSonal <sonali.mandrupkar@gmail.com>
2025-07-11 16:40:42 +0530

    checking resource existance

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,13 +1,30 @@
+# Lookup for stable Amazon Linux 2 AMI (HVM, EBS-backed, x86_64)
 data "aws_ami" "amazon_linux" {
+  most_recent = true
   owners      = ["amazon"]
-  most_recent = false # changed to prevent drifting on AMI updates
 
   filter {
     name   = "name"
-    values = ["amzn2-ami-hvm-2.0.????????*-x86_64-gp2"]
+    values = ["amzn2-ami-hvm-2.0.*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
 }
 
+# Generate EC2 user data with DB credentials injected
 data "template_file" "user_data" {
   template = file("${path.module}/setup-ec2.sh")
 
@@ -18,9 +35,9 @@ data "template_file" "user_data" {
   }
 }
 
-# IAM Role for EC2 to access SSM
+# IAM role to allow EC2 to access SSM parameters
 resource "aws_iam_role" "ec2_ssm_role" {
-  count = var.create_iam_role ? 1 : 0
+  count = var.create_iam_role && var.create_ec2 ? 1 : 0
 
   name = "shiptivitas-ec2-ssm-role"
 
@@ -36,25 +53,30 @@ resource "aws_iam_role" "ec2_ssm_role" {
   })
 }
 
+# Attach AmazonSSM policies
 resource "aws_iam_role_policy_attachment" "ssm_policy" {
-  count      = var.create_iam_role ? 1 : 0
+  count      = var.create_iam_role && var.create_ec2 ? 1 : 0
   role       = aws_iam_role.ec2_ssm_role[0].name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMReadOnlyAccess"
 }
 
 resource "aws_iam_role_policy_attachment" "ssm_core" {
-  count      = var.create_iam_role ? 1 : 0
+  count      = var.create_iam_role && var.create_ec2 ? 1 : 0
   role       = aws_iam_role.ec2_ssm_role[0].name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
+# Instance profile to bind role to EC2
 resource "aws_iam_instance_profile" "ec2_ssm_profile" {
-  count = var.create_iam_profile ? 1 : 0
+  count = var.create_iam_profile && var.create_ec2 ? 1 : 0
   name  = "shiptivitas-ec2-ssm-profile"
   role  = var.create_iam_role ? aws_iam_role.ec2_ssm_role[0].name : "shiptivitas-ec2-ssm-role"
 }
 
+# EC2 Instance to run backend API
 resource "aws_instance" "shiptivitas_api" {
+  count = var.create_ec2 ? 1 : 0
+
   ami                         = data.aws_ami.amazon_linux.id
   instance_type               = "t2.micro"
   key_name                    = var.key_name
@@ -67,7 +89,7 @@ resource "aws_instance" "shiptivitas_api" {
   lifecycle {
     create_before_destroy = true
     ignore_changes = [
-      ami, # Prevent unnecessary recreation when AMI updates
+      ami,
       user_data
     ]
   }
