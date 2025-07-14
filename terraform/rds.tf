02556c5a44e93dde4b7cada86aa4b49617eb2580
RareSonal <sonali.mandrupkar@gmail.com>
2025-07-14 21:45:33 +0530

    updated

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,18 +1,27 @@
+# Lookup existing RDS instance
 data "aws_db_instance" "existing_rds" {
   db_instance_identifier = trimspace(var.db_instance_identifier)
 }
 
+# Fetch DB username from SSM Parameter Store
 data "aws_ssm_parameter" "db_username" {
   name            = trimspace(var.db_username_ssm_path)
   with_decryption = true
 }
 
+# Fetch DB password from SSM Parameter Store
 data "aws_ssm_parameter" "db_password" {
   name            = trimspace(var.db_password_ssm_path)
   with_decryption = true
 }
 
+# Allow EC2 to connect to RDS via port 5432 (PostgreSQL)
 resource "aws_security_group_rule" "allow_ec2_to_rds" {
+  count = (
+    trimspace(var.rds_security_group_id) != "" &&
+    trimspace(var.ec2_security_group_id) != "" ? 1 : 0
+  )
+
   type                     = "ingress"
   from_port                = 5432
   to_port                  = 5432
@@ -20,4 +29,9 @@ resource "aws_security_group_rule" "allow_ec2_to_rds" {
   security_group_id        = var.rds_security_group_id
   source_security_group_id = var.ec2_security_group_id
   description              = "Allow EC2 instances to access RDS PostgreSQL"
+
+  lifecycle {
+    create_before_destroy = true
+    ignore_changes        = [description]
+  }
 }
