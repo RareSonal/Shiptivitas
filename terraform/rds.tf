6014f5a61a7f0e80c28ac6bc6b2dcf5da316f287
RareSonal <sonali.mandrupkar@gmail.com>
2025-07-14 19:19:59 +0530

    added rule to connect ec2 sg to rds sg

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -11,3 +11,13 @@ data "aws_ssm_parameter" "db_password" {
   name            = trimspace(var.db_password_ssm_path)
   with_decryption = true
 }
+
+resource "aws_security_group_rule" "allow_ec2_to_rds" {
+  type                     = "ingress"
+  from_port                = 5432
+  to_port                  = 5432
+  protocol                 = "tcp"
+  security_group_id        = var.rds_security_group_id
+  source_security_group_id = var.ec2_security_group_id
+  description              = "Allow EC2 instances to access RDS PostgreSQL"
+}
