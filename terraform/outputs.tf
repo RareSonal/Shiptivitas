15166911e8c1c568f1094634f9d9a20b6552c66e
RareSonal <sonali.mandrupkar@gmail.com>
2025-07-23 16:08:30 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,23 +1,31 @@
 output "s3_bucket" {
   description = "Name of the S3 bucket (if created)"
-  value       = var.create_s3_bucket && length(aws_s3_bucket.shiptivitas_frontend) > 0 ? aws_s3_bucket.shiptivitas_frontend[0].bucket : null
+  value = (
+    var.create_s3_bucket && length(aws_s3_bucket.shiptivitas_frontend) > 0
+    ? aws_s3_bucket.shiptivitas_frontend[0].bucket
+    : null
+  )
 }
 
 output "cloudfront_url" {
   description = "CloudFront distribution URL (existing or new)"
-  value       = var.use_existing_cdn ? try(data.aws_cloudfront_distribution.existing[0].domain_name, null) : try(aws_cloudfront_distribution.cdn[0].domain_name, null)
+  value = (
+    var.use_existing_cdn
+    ? try(data.aws_cloudfront_distribution.existing[0].domain_name, null)
+    : try(aws_cloudfront_distribution.cdn[0].domain_name, null)
+  )
 }
 
 output "backend_ec2_public_ip" {
   description = "Public IP of EC2 instance (if created)"
-  value = try(
-    length(aws_instance.shiptivitas_api) > 0 ?
-      aws_instance.shiptivitas_api[0].public_ip : null,
-    null
+  value = (
+    length(aws_instance.shiptivitas_api) > 0
+    ? try(aws_instance.shiptivitas_api[0].public_ip, null)
+    : null
   )
 }
 
 output "rds_endpoint" {
   description = "RDS endpoint"
-  value       = data.aws_db_instance.existing_rds.address
+  value       = try(data.aws_db_instance.existing_rds.address, null)
 }
