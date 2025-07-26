a80ddd0ce5ad07329c6e10d345bd34569887b900
RareSonal <sonali.mandrupkar@gmail.com>
2025-07-26 21:14:29 +0530

    Update HomeTab.js

--- frontend/src/HomeTab.js
+++ frontend/src/HomeTab.js
@@ -1,5 +1,6 @@
-// HomeTab.js
 import React, { useState } from 'react';
+
+const apiBaseUrl = process.env.REACT_APP_API_BASE_URL;
 
 export default function HomeTab({ onUnlock }) {
   const [pin, setPin] = useState('');
@@ -20,7 +21,7 @@ export default function HomeTab({ onUnlock }) {
     setIsLoading(true);
 
     try {
-      const response = await fetch('ec2-3.81.143.88.compute-1.amazonaws.com/api/verify-pin', {
+      const response = await fetch(`${apiBaseUrl}/api/verify-pin`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ pin }),
@@ -33,56 +34,4 @@ export default function HomeTab({ onUnlock }) {
       } else {
         setErrorMessage(data.error || 'Invalid PIN.');
       }
-    } catch (error) {
-      console.error('Error verifying PIN:', error);
-      setErrorMessage('An error occurred while verifying the PIN.');
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  return (
-    <header className="App-header">
-      <div className="App-title">
-        <h1>SHIPTIVITAS</h1>
-        <div className="App-icon">
-          <div className="Circle"></div>
-        </div>
-      </div>
-
-      <div className="pin-input-container" style={{ marginTop: '20px' }}>
-        <h3>Enter your 4-digit PIN to unlock Shipping Requests Tab</h3>
-        <input
-          type="password" // Use "password" to hide input characters
-          value={pin}
-          onChange={handlePinChange}
-          maxLength="4"
-          placeholder="PIN"
-          style={{
-            padding: '8px',
-            fontSize: '16px',
-            width: '100px',
-            textAlign: 'center',
-          }}
-        />
-        <button
-          onClick={handleUnlockClick}
-          style={{ marginLeft: '10px', padding: '8px 16px', fontSize: '16px' }}
-          disabled={isLoading}
-        >
-          {isLoading ? 'Verifying...' : 'Unlock'}
-        </button>
-        {errorMessage && (
-          <div
-            className="error-message"
-            style={{ color: 'red', marginTop: '10px' }}
-          >
-            {errorMessage}
-          </div>
-        )}
-      </div>
-    </header>
-  );
-}
-
-
+    } catch (error
